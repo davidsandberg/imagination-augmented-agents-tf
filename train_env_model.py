5c339d0c03ba54eb9814aa83c310db31af73314f
David Sandberg <david.o.sandberg@gmail.com>
2019-01-06 14:40:59 +0100

    Added static unrolling of graph

--- train_env_model.py
+++ train_env_model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.python.ops import variable_scope as vs
 import numpy as np
 import pickle
 
@@ -59,11 +60,12 @@ def state_transition_module(a, s, z):
 def observation_encoder(o):
     """
     """
-    std1 = tf.nn.space_to_depth(o, 4)
-    cs1 = conv_stack(std1, 3, 16, 5, 16, 3, 64)
-    std2 = tf.nn.space_to_depth(cs1, 2)
-    cs2 = conv_stack(std2, 3, 32, 5, 32, 3, 64)
-    e = tf.nn.relu(cs2)
+    with tf.variable_scope('observation_encoder_module'):
+        std1 = tf.nn.space_to_depth(o, 4)
+        cs1 = conv_stack(std1, 3, 16, 5, 16, 3, 64)
+        std2 = tf.nn.space_to_depth(cs1, 2)
+        cs2 = conv_stack(std2, 3, 32, 5, 32, 3, 64)
+        e = tf.nn.relu(cs2)
     return e
 
 def prior_module(s, a):
@@ -91,21 +93,21 @@ def initial_state_module(ex, nrof_observations=3):
     that the first dimension should be of size batch_size x nrof_observations.
     """
     with tf.variable_scope('initial_state_module'):
-        shape = [-1,nrof_observations]+ex.get_shape().as_list()[1:]
-        e = tf.unstack(tf.reshape(ex, shape), axis=1)
+        e = tf.unstack(ex, axis=1)
         c = tf.concat(e, axis=-1)
         s = conv_stack(c, 1, 64, 3, 64, 3, 64)
     return s
 
 def observation_decoder(s, z):
-    if z is None:
-        c = s
-    else:
-        c = tf.concat([s, z], axis=-1)
-    cs1 = conv_stack(c, 1, 32, 5, 32, 3, 64)
-    dts1 = tf.nn.depth_to_space(cs1, 2)
-    cs2 = conv_stack(dts1, 3, 64, 3, 64, 1, 48)
-    dts2 = tf.nn.depth_to_space(cs2, 4)
+    with tf.variable_scope('observation_decoder_module'):
+        if z is None:
+            c = s
+        else:
+            c = tf.concat([s, z], axis=-1)
+        cs1 = conv_stack(c, 1, 32, 5, 32, 3, 64)
+        dts1 = tf.nn.depth_to_space(cs1, 2)
+        cs2 = conv_stack(dts1, 3, 64, 3, 64, 1, 48)
+        dts2 = tf.nn.depth_to_space(cs2, 4)
     return dts2
 
 def kl_divergence_gaussians(mu0, log_sigma0, mu1, log_sigma1):
@@ -116,56 +118,84 @@ def kl_divergence_gaussians(mu0, log_sigma0, mu1, log_sigma1):
     sigma0 = tf.exp(log_sigma0)
     sigma1 = tf.exp(log_sigma1)
     k = 0.0
-    tf1 = tf.reduce_sum(1.0/sigma1 * sigma0)
-    tf2 = tf.reduce_sum((mu1-mu0)*1.0/sigma1*(mu1-mu0))
-    tf3 = tf.cast(tf.log(tf.reduce_prod(sigma1/sigma0)), tf.float32)
+    tf1 = tf.reduce_sum(1.0/sigma1 * sigma0, axis=[2,3,4])
+    tf2 = tf.reduce_sum((mu1-mu0)*1.0/sigma1*(mu1-mu0), axis=[2,3,4])
+    tf3 = tf.cast(tf.log(tf.reduce_prod(sigma1/sigma0, axis=[2,3,4])), tf.float32)
     Dkl = 0.5 * (tf1 + tf2 - k + tf3)
     return Dkl
 
 def cross_entropy(labels, logits):
-    xent = -tf.reduce_sum(labels * tf.log(logits), [0, 1, 2, 3])
+    xent = -tf.reduce_sum(labels * tf.log(logits), axis=[2,3,4])
     return xent
-
+  
 class EnvModel():
     
-    def __init__(self, obs_shape, num_actions):
-        width, height, depth = obs_shape
+    def __init__(self, obs_shape, num_actions, nrof_time_steps):
+        length, width, height, depth = obs_shape
+        nrof_init_time_steps = 3
     
-        self.obs_init = tf.placeholder(tf.float32, [None, width, height, depth])  # TODO: Fix dimensions with batches
-        self.obs = tf.placeholder(tf.float32, [None, width, height, depth])
-        self.actions = tf.placeholder(tf.int32, [None,])
+        self.obs = tf.placeholder(tf.float32, [None, length, width, height, depth])
+        self.actions = tf.placeholder(tf.int32, [None, length])
         
-        assert_op = tf.Assert(tf.equal(tf.shape(self.obs_init)[0], tf.shape(self.obs)[0]*3), [self.obs_init, self.obs])
-        with tf.control_dependencies([assert_op]):
-            # Make sure that parameters for the observation encoder are shared
-            with tf.variable_scope('observation_encoder', reuse=False):
-                encoded_obs = observation_encoder(self.obs)
-            with tf.variable_scope('observation_encoder', reuse=True):
-                encoded_init_obs = observation_encoder(self.obs_init)
-            
+        # Encode observations
+        obs_reshaped = tf.reshape(self.obs, [-1, width, height, depth])
+        encoded_obs_reshaped = observation_encoder(obs_reshaped)
+        shape = [-1,length ]+encoded_obs_reshaped.get_shape().as_list()[1:]
+        encoded_obs = tf.reshape(encoded_obs_reshaped, shape)
+        
         # Initialize state
-        self.state = initial_state_module(encoded_init_obs)
+        encoded_obs_init = encoded_obs[:,:nrof_init_time_steps,:,:,:]
+        state = initial_state_module(encoded_obs_init)
         
         # Convert actions to one-hot
-        qq = tf.one_hot(tf.reshape(self.actions, [-1, 1, 1]), num_actions, axis=-1)
-        onehot_actions = tf.tile(qq, multiples=(1, self.state.get_shape()[1], self.state.get_shape()[2], 1))
+        qq = tf.one_hot(tf.reshape(self.actions, [-1, length, 1, 1]), num_actions, axis=-1)
+        onehot_actions = tf.tile(qq, multiples=(1, 1, state.get_shape()[1], state.get_shape()[2], 1))
+        obs_hat_list = []
+        next_state_list = []
+        reg_loss_list = []
+        mu_list = []
+        sigma_list = []
+        mu_hat_list = []
+        sigma_hat_list = []
+        for t in range(nrof_time_steps):
+          
+            if t > 0:
+                tf.get_variable_scope().reuse_variables()
+          
+            # Compute statistics for prior and posterior
+            mu, sigma = prior_module(state, onehot_actions[:,t,:,:,:])
+            mu_hat, sigma_hat = posterior_module(mu, sigma, state, encoded_obs[:,t,:,:,:], onehot_actions[:,t,:,:,:])
+            mu_list += [ mu ]
+            sigma_list += [ sigma ]
+            mu_hat_list += [ mu_hat ]
+            sigma_hat_list += [ sigma_hat ]
+            
+            # Sample from z using the reparametrization trick
+            eps = tf.random_normal(tf.shape(sigma_hat), 0.0, 1.0, dtype=tf.float32)
+            z = mu_hat + tf.multiply(tf.exp(sigma_hat), eps)  # Check how standard deviation is represented in the paper
+            
+            # Calculate next state
+            next_state = state_transition_module(onehot_actions[:,t,:,:,:], state, z)
+            next_state_list += [ next_state ]
+            
+            # Calculate observation
+            obs_hat = observation_decoder(next_state, z)
+            obs_hat_list += [ obs_hat ]
+            
+            
+            state = next_state
+            
+        self.next_state = tf.stack(next_state_list, axis=1)
+        self.obs_hat = tf.stack(obs_hat_list, axis=1)
         
-        # Compute statistics for prior and posterior
-        mu, sigma = prior_module(self.state, onehot_actions)
-        mu_hat, sigma_hat = posterior_module(mu, sigma, self.state, encoded_obs, onehot_actions)
+        # Calculate loss
+        self.mu = tf.stack(mu_list, axis=1)
+        self.sigma = tf.stack(sigma_list, axis=1)
+        self.mu_hat = tf.stack(mu_hat_list, axis=1)
+        self.sigma_hat = tf.stack(sigma_hat_list, axis=1)
+        self.regularization_loss = kl_divergence_gaussians(self.mu, self.sigma, self.mu_hat, self.sigma_hat)
+        self.reconstruction_loss = cross_entropy(self.obs[:,nrof_init_time_steps:,:,:,:], self.obs_hat)  # Should be KL divergence according to paper
         
-        # Sample from z using the reparametrization trick
-        eps = tf.random_normal(tf.shape(sigma_hat), 0.0, 1.0, dtype=tf.float32)
-        z = mu_hat + tf.multiply(tf.exp(sigma_hat), eps)  # Check how standard deviation is represented in the paper
-        
-        self.next_state = state_transition_module(onehot_actions, self.state, z)
-        self.obs_hat = observation_decoder(self.next_state, z)
-        
-        self.regularization_loss = kl_divergence_gaussians(mu, sigma, mu_hat, sigma_hat)
-        self.reconstruction_loss = cross_entropy(self.obs, self.obs_hat)  # Should be KL divergence according to paper
-        self.loss = self.regularization_loss + self.reconstruction_loss
-        
-        self.train_op = tf.train.AdamOptimizer().minimize(self.loss)
     
 def load_pickle(filename):
     with open(filename, 'rb') as f:
@@ -176,7 +206,12 @@ if __name__ == '__main__':
 
     with tf.Session() as sess:
         with tf.variable_scope('env_model'):
-            env_model = EnvModel((80, 80, 3), 1)
+            env_model = EnvModel((13, 80, 80, 3), 1, 10)
+
+        reg_loss = tf.reduce_sum(env_model.regularization_loss)
+        rec_loss = tf.reduce_sum(env_model.reconstruction_loss)
+        loss = reg_loss + rec_loss
+        env_model.train_op = tf.train.AdamOptimizer().minimize(loss)
 
         sess.run(tf.global_variables_initializer())
 
@@ -184,13 +219,16 @@ if __name__ == '__main__':
         train = np.expand_dims(train, 4)
         train = np.repeat(train, 3, axis=4)
         #print(train.shape)
-        obs_init = train[0:5,:3,:,:,:].reshape(15,80,80,3)
-        obs = train[0:5,3,:,:,:]
-        actions = np.zeros((5,), dtype=np.float32)
+        obs = train[0:2,:13,:,:,:]
+        actions = np.zeros((2,13), dtype=np.float32)
         
-        feed_dict = {env_model.obs_init:obs_init, env_model.obs:obs, env_model.actions:actions}
-        _, obs_hat_, next_state_ = sess.run([env_model.train_op, env_model.obs_hat, env_model.next_state], feed_dict=feed_dict)
+        print('Training')
+        feed_dict = {env_model.obs:obs, env_model.actions:actions}
+        _, obs_hat_, next_state_, rec_loss_, reg_loss_, loss_ = sess.run([env_model.train_op, env_model.obs_hat, env_model.next_state, rec_loss, reg_loss, loss], feed_dict=feed_dict)
         
-        print('Next state shape: ', next_state_.shape)
-        print('Generated observation shape: ', obs_hat_.shape)
+        print('Reconstruction loss: ', rec_loss_)
+        print('Regularization loss: ', reg_loss_)
+        print('Total loss: ', loss_)
+#        print('Next state shape: ', next_state_.shape)
+#        print('Generated observation shape: ', obs_hat_.shape)
         
