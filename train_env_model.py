4232942edaf4e7c10f624602e66f9aa82b79e928
David Sandberg <david.o.sandberg@gmail.com>
2019-01-08 21:14:29 +0100

    Debugging of loss function. Fixed problem with NaNs.

--- train_env_model.py
+++ train_env_model.py
@@ -1,35 +1,35 @@
 import tensorflow as tf
-from tensorflow.python.ops import variable_scope as vs
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
 
-def conv_stack(X, k1, c1, k2, c2, k3, c3):
+def conv_stack(X, k1, c1, k2, c2, k3, c3, initializer=tf.initializers.truncated_normal(0.00000001)):
     """Implements the conv_stack module as described in figure 6 in the paper
     """
-    conv1 = tf.layers.conv2d(X, c1, kernel_size=k1,
-            strides=1, padding='same', activation=None)
+    conv1 = tf.contrib.layers.conv2d(X, num_outputs=c1, kernel_size=k1,
+            stride=1, padding='same', activation_fn=None)
     conv1_relu = tf.nn.relu(conv1)
-    conv2 = tf.layers.conv2d(conv1_relu, c2, kernel_size=k2,
-            strides=1, padding='same', activation=None)
+    conv2 = tf.contrib.layers.conv2d(conv1_relu, num_outputs=c2, kernel_size=k2,
+            stride=1, padding='same', activation_fn=None)
     conv2_relu = tf.nn.relu(conv1_relu + conv2)
-    conv3 = tf.layers.conv2d(conv2_relu, c3, kernel_size=k3,
-            strides=1, padding='same', activation=None)
+    conv3 = tf.contrib.layers.conv2d(conv2_relu, num_outputs=c3, kernel_size=k3,
+            stride=1, padding='same', activation_fn=None)
     return conv3
 
 def res_conv(X, use_extra_convolution=True):
     """Implements the res_conv module as described in figure 7 in the paper
     """
     if use_extra_convolution:
-        c = tf.layers.conv2d(X, 64, kernel_size=1,
-                strides=1, padding='same', activation=None)  #  shape=(?, 10, 10, 64)
+        c = tf.contrib.layers.conv2d(X, num_outputs=64, kernel_size=1,
+                stride=1, padding='same', activation_fn=None)  #  shape=(?, 10, 10, 64)
     else:
         c = X
-    rc1_relu = tf.layers.conv2d(c, 32, kernel_size=3,
-            strides=1, padding='same', activation=tf.nn.relu)
-    rc2_relu = tf.layers.conv2d(rc1_relu, 32, kernel_size=5,
-            strides=1, padding='same', activation=tf.nn.relu)
-    rc3 = tf.layers.conv2d(rc2_relu, 64, kernel_size=3,
-            strides=1, padding='same', activation=None)
+    rc1_relu = tf.contrib.layers.conv2d(c, num_outputs=32, kernel_size=3,
+            stride=1, padding='same', activation_fn=tf.nn.relu)
+    rc2_relu = tf.contrib.layers.conv2d(rc1_relu, num_outputs=32, kernel_size=5,
+            stride=1, padding='same', activation_fn=tf.nn.relu)
+    rc3 = tf.contrib.layers.conv2d(rc2_relu, num_outputs=64, kernel_size=3,
+            stride=1, padding='same', activation_fn=None)
     rc = c + rc3
     return rc
 
@@ -86,7 +86,7 @@ def posterior_module(mu, sigma, s, e, a):
         sigma_hat = tf.nn.softplus(conv_stack(c, 1, 32, 3, 32, 3, 64))
     return mu_hat, sigma_hat
 
-def initial_state_module(ex, nrof_observations=3):
+def initial_state_module(ex):
     """Computes the initial state from the feature maps of a
     number of consequtive initial observations. 
     These feature maps are given in the batch dimension, meaning
@@ -110,23 +110,54 @@ def observation_decoder(s, z):
         dts2 = tf.nn.depth_to_space(cs2, 4)
     return dts2
 
-def kl_divergence_gaussians(mu0, log_sigma0, mu1, log_sigma1):
-    '''Implements KL divergence between two Gaussians with diagonal covariance matrices based on
-    https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
-    The prior evolves over time and will hence not be restricted to N(0,1)
-    '''
-    sigma0 = tf.exp(log_sigma0)
-    sigma1 = tf.exp(log_sigma1)
-    k = 0.0
-    tf1 = tf.reduce_sum(1.0/sigma1 * sigma0, axis=[2,3,4])
-    tf2 = tf.reduce_sum((mu1-mu0)*1.0/sigma1*(mu1-mu0), axis=[2,3,4])
-    tf3 = tf.cast(tf.log(tf.reduce_prod(sigma1/sigma0, axis=[2,3,4])), tf.float32)
-    Dkl = 0.5 * (tf1 + tf2 - k + tf3)
-    return Dkl
+# def kl_divergence_gaussians(mu0, log_sigma0, mu1, log_sigma1):
+#     '''Implements KL divergence between two Gaussians with diagonal covariance matrices based on
+#     https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
+#     The prior evolves over time and will hence not be restricted to N(0,1)
+#     '''
+#     sigma0 = tf.exp(log_sigma0)
+#     sigma1 = tf.exp(log_sigma1)
+#     k = 0.0
+#     tf1 = tf.reduce_sum(1.0/sigma1 * sigma0, axis=[2,3,4])
+#     tf2 = tf.reduce_sum((mu1-mu0)*1.0/sigma1*(mu1-mu0), axis=[2,3,4])
+#     tf3 = tf.cast(tf.log(tf.reduce_prod(sigma1/sigma0, axis=[2,3,4])), tf.float32)
+#     Dkl = 0.5 * (tf1 + tf2 - k + tf3)
+#     return Dkl
+  
+# def kl_divergence_gaussians(p_mu, p_sigma_log, q_mu, q_sigma_log):
+#     zz = tf.distributions.kl_divergence(
+#     tf.distributions.Normal(loc=q_mu, scale=tf.exp(q_sigma_log)+1e-4),
+#     tf.distributions.Normal(loc=p_mu, scale=tf.exp(p_sigma_log)+1e-4))
+#     return tf.reduce_sum(zz, axis=[2,3,4]), zz
+  
+def kl_divergence_gaussians(p_mu, p_sigma, q_mu, q_sigma):
+    eps = 1e-1
+    zz = tf.distributions.kl_divergence(
+    tf.distributions.Normal(loc=q_mu, scale=q_sigma+eps),
+    tf.distributions.Normal(loc=p_mu, scale=p_sigma+eps))
+    return tf.reduce_sum(zz, axis=[2,3,4]), zz
+  
+def kl_divergence_gaussians2(p_mu, p_sigma_log, q_mu, q_sigma_log):
+    #https://hk.saowen.com/a/7404b78cd5b980e16e08423192e35ec18ecb3cb243d310a9d9a194747d9ee1ba
+    eps = 1e-3
+    r = q_mu - p_mu
+    p_sigma, q_sigma = tf.exp(p_sigma_log), tf.exp(q_sigma_log)
+    #zz = tf.log(p_sigma) - tf.log(q_sigma) - .5 * (1. - (q_sigma**2 + r**2) / (p_sigma**2+eps))
+    zz = p_sigma_log - q_sigma_log - .5 * (1. - (q_sigma**2 + r**2) / (p_sigma**2+eps))
+    return tf.reduce_sum(zz, axis=[2,3,4]), zz
 
 def cross_entropy(labels, logits):
-    xent = -tf.reduce_sum(labels * tf.log(logits), axis=[2,3,4])
+    #xent = -tf.reduce_sum(labels * tf.log(logits), axis=[2,3,4])
+    xent = -tf.reduce_sum(labels * logits, axis=[2,3,4])
     return xent
+  
+def format_mmm(x):
+    min_str, max_str, mean_str = [], [], []
+    for i in range(x.shape[1]):
+        min_str += [ '%.3g' % np.min(x[:,i,:,:,:]) ]
+        max_str += [ '%.3g' % np.max(x[:,i,:,:,:]) ]
+        mean_str += [ '%.3g' % np.mean(x[:,i,:,:,:]) ]
+    return ', '.join(min_str), ', '.join(max_str), ', '.join(mean_str)
   
 class EnvModel():
     
@@ -139,24 +170,26 @@ class EnvModel():
         
         # Encode observations
         obs_reshaped = tf.reshape(self.obs, [-1, width, height, depth])
-        encoded_obs_reshaped = observation_encoder(obs_reshaped)
-        shape = [-1,length ]+encoded_obs_reshaped.get_shape().as_list()[1:]
-        encoded_obs = tf.reshape(encoded_obs_reshaped, shape)
+        self.encoded_obs_reshaped = observation_encoder(obs_reshaped)
+        
+        shape = [-1,length]+self.encoded_obs_reshaped.get_shape().as_list()[1:]
+        self.encoded_obs = tf.reshape(self.encoded_obs_reshaped, shape)
         
         # Initialize state
-        encoded_obs_init = encoded_obs[:,:nrof_init_time_steps,:,:,:]
-        state = initial_state_module(encoded_obs_init)
+        self.encoded_obs_init = self.encoded_obs[:,:nrof_init_time_steps,:,:,:]
+        self.initial_state = initial_state_module(self.encoded_obs_init)
+        state = self.initial_state
         
         # Convert actions to one-hot
         qq = tf.one_hot(tf.reshape(self.actions, [-1, length, 1, 1]), num_actions, axis=-1)
         onehot_actions = tf.tile(qq, multiples=(1, 1, state.get_shape()[1], state.get_shape()[2], 1))
         obs_hat_list = []
         next_state_list = []
-        reg_loss_list = []
         mu_list = []
         sigma_list = []
         mu_hat_list = []
         sigma_hat_list = []
+        z_list = []
         for t in range(nrof_time_steps):
           
             if t > 0:
@@ -164,7 +197,7 @@ class EnvModel():
           
             # Compute statistics for prior and posterior
             mu, sigma = prior_module(state, onehot_actions[:,t,:,:,:])
-            mu_hat, sigma_hat = posterior_module(mu, sigma, state, encoded_obs[:,t,:,:,:], onehot_actions[:,t,:,:,:])
+            mu_hat, sigma_hat = posterior_module(mu, sigma, state, self.encoded_obs[:,t,:,:,:], onehot_actions[:,t,:,:,:])
             mu_list += [ mu ]
             sigma_list += [ sigma ]
             mu_hat_list += [ mu_hat ]
@@ -172,7 +205,9 @@ class EnvModel():
             
             # Sample from z using the reparametrization trick
             eps = tf.random_normal(tf.shape(sigma_hat), 0.0, 1.0, dtype=tf.float32)
-            z = mu_hat + tf.multiply(tf.exp(sigma_hat), eps)  # Check how standard deviation is represented in the paper
+            #z = mu_hat + tf.multiply(tf.exp(sigma_hat), eps)  # Check how standard deviation is represented in the paper
+            z = mu_hat + tf.multiply(sigma_hat, eps)  # Check how standard deviation is represented in the paper
+            z_list += [ z ]
             
             # Calculate next state
             next_state = state_transition_module(onehot_actions[:,t,:,:,:], state, z)
@@ -193,7 +228,8 @@ class EnvModel():
         self.sigma = tf.stack(sigma_list, axis=1)
         self.mu_hat = tf.stack(mu_hat_list, axis=1)
         self.sigma_hat = tf.stack(sigma_hat_list, axis=1)
-        self.regularization_loss = kl_divergence_gaussians(self.mu, self.sigma, self.mu_hat, self.sigma_hat)
+        self.z = tf.stack(z_list, axis=1)
+        self.regularization_loss, self.zz = kl_divergence_gaussians(self.mu, self.sigma, self.mu_hat, self.sigma_hat)
         self.reconstruction_loss = cross_entropy(self.obs[:,nrof_init_time_steps:,:,:,:], self.obs_hat)  # Should be KL divergence according to paper
         
     
@@ -207,6 +243,7 @@ if __name__ == '__main__':
     with tf.Session() as sess:
         with tf.variable_scope('env_model'):
             env_model = EnvModel((13, 80, 80, 3), 1, 10)
+            #env_model = EnvModel((13, 80, 80, 3), 1, 10)
 
         reg_loss = tf.reduce_sum(env_model.regularization_loss)
         rec_loss = tf.reduce_sum(env_model.reconstruction_loss)
@@ -218,17 +255,28 @@ if __name__ == '__main__':
         train = load_pickle('bouncing_balls_testing_data.pkl')
         train = np.expand_dims(train, 4)
         train = np.repeat(train, 3, axis=4)
-        #print(train.shape)
-        obs = train[0:2,:13,:,:,:]
-        actions = np.zeros((2,13), dtype=np.float32)
+        obs = train[0:10,:13,:,:,:]
+        actions = np.zeros((10,13), dtype=np.float32)
+        m = env_model
         
         print('Training')
         feed_dict = {env_model.obs:obs, env_model.actions:actions}
-        _, obs_hat_, next_state_, rec_loss_, reg_loss_, loss_ = sess.run([env_model.train_op, env_model.obs_hat, env_model.next_state, rec_loss, reg_loss, loss], feed_dict=feed_dict)
+        #_, obs_hat_, next_state_, rec_loss_, reg_loss_, loss_ = sess.run([env_model.train_op, env_model.obs_hat, env_model.next_state, rec_loss, reg_loss, loss], feed_dict=feed_dict)
+        obs_hat_, initial_state_, next_state_, reg_loss_, rec_loss_, eoi_, mu_, sigma_, mu_hat_, sigma_hat_, zz_, encoded_obs_, z_ = sess.run(
+          [m.obs_hat, m.initial_state, m.next_state, m.regularization_loss, m.reconstruction_loss, m.encoded_obs_init, m.mu, m.sigma, m.mu_hat, m.sigma_hat, m.zz, m.encoded_obs, m.z], feed_dict=feed_dict)
         
-        print('Reconstruction loss: ', rec_loss_)
-        print('Regularization loss: ', reg_loss_)
-        print('Total loss: ', loss_)
-#        print('Next state shape: ', next_state_.shape)
-#        print('Generated observation shape: ', obs_hat_.shape)
+        print('obs:              (%s), (%s), (%s)' % format_mmm(obs))
+        print('encoded obs: (%s), (%s), (%s)' % format_mmm(encoded_obs_))
+        #print('initial_state:    (%s), (%s), (%s)' % format_mmm(initial_state_))
+        print('mu:               (%s), (%s), (%s)' % format_mmm(mu_))
+        print('sigma:            (%s), (%s), (%s)' % format_mmm(sigma_))
+        print('mu_hat:           (%s), (%s), (%s)' % format_mmm(mu_hat_))
+        print('sigma_hat:        (%s), (%s), (%s)' % format_mmm(sigma_hat_))
+        print('z:                (%s), (%s), (%s)' % format_mmm(z_))
+        print('next_state:       (%s), (%s), (%s)' % format_mmm(next_state_))
+        print('obs_hat:          (%s), (%s), (%s)' % format_mmm(obs_hat_))
+        
+        print('Reconstruction loss: ', rec_loss_[0,:])
+        print('Regularization loss: ', reg_loss_[0,:])
+        #print('Total loss: ', loss_)
         
